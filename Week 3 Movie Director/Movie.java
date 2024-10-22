9803e54b8c4737e2a1c3f59380f65cc96053aa9d
W R Aloka Pathiraja <w2052008@westminster.ac.uk>
2024-10-22 10:10:11 +0530

    Update Movie.java

--- Week 3 Movie Director/Movie.java
+++ Week 3 Movie Director/Movie.java
@@ -1,13 +1,16 @@
+// Modified Movie class to include Actor list
 public class Movie {
     private String title;
     private String category;
     private int numAwards;
     private Director director;
+    private Actor[] listActor;
 
-    public Movie(String title, String category, Director director) {
+    public Movie(String title, String category, Director director, int numActors) {
         this.title = title;
-        this.category= category;
+        this.category = category;
         this.director = director;
+        this.listActor = new Actor[numActors];  // Initialize the actor array
     }
 
     public String getTitle() {
@@ -30,14 +33,28 @@ public class Movie {
         this.numAwards = numAwards;
     }
 
+    public Actor[] getListActor() {
+        return listActor;
+    }
+
+    public void setActor(Actor actor, int index) {
+        this.listActor[index] = actor;
+    }
+
     @Override
     public String toString() {
+        StringBuilder actorList = new StringBuilder();
+        for (Actor actor : listActor) {
+            if (actor != null) {
+                actorList.append(actor.toString()).append("\n");
+            }
+        }
         return "Movie{" +
                 "title='" + title + '\'' +
                 ", category='" + category + '\'' +
                 ", numAwards=" + numAwards +
                 ", director=" + director +
+                ", listActor=\n" + actorList.toString() +
                 '}';
     }
 }
-
